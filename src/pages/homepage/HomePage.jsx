df511e260d5ba75885f5f7ba4d10f7ad97625e98
ZoraidaMorenoCadenas <zoraidamorenocadenas79@gmail.com>
2023-08-29 15:24:28 +0200

    home page and call axios to back end

---
+++ src/pages/homepage/HomePage.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect} from 'react'
+import Card from '../../components/card/Card';
+import { CardService } from '../../services/Cards-Service';
+
+function HomePage() {
+  const [cards, setCards] = useState([]);
+    const cardService = CardService();
+    
+    useEffect(() => {
+        cardService.getAll()
+           
+            .then (function (response){
+                console.log(response);
+                setCards(response.data);
+            })
+            .catch(function (error) {
+            console.log(error);
+            })
+            .finally(function () {
+            });
+        // El código aquí se ejecutará después de *cada* renderizado
+    }, []);
+
+  return (
+    <>
+
+<div className="container-cards-grid">
+         {cards.map((card) => (
+            <Card
+            key={card.id}
+            id= {card.id}
+            title = {card.title}
+            location = {card.location}
+            image= {card.image} />
+         ))}
+            {/*<button onClick={handleAdd}>Agregar</button>*/}
+         </div>
+      <h1>HOLA</h1>
+
+
+
+
+    </>
+  )
+}
+
+export default HomePage
