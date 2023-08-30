2bd283bc88ceea9a79f6966167664733e1a7f3f2
Sylvia Llorente <sylviall81@gmail.com>
2023-08-30 18:36:13 +0200

    card dashboard

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,44 +1,15 @@
 import { useState, useEffect} from 'react'
-import Card from '../../components/card/Card';
 import { CardService } from '../../services/Cards-Service';
 import CardDashboard from '../../components/card-dashboard/Card-dashboard';
+import Header from '../../components/header/Header';
 
 function HomePage() {
-  const [cards, setCards] = useState([]);
-    const cardService = CardService();
-    
-    useEffect(() => {
-        cardService.getAll()
-           
-            .then (function (response){
-                //console.log(response);
-                setCards(response.data);
-            })
-            .catch(function (error) {
-            console.log(error);
-            })
-            .finally(function () {
-            });
-        // El código aquí se ejecutará después de *cada* renderizado
-    }, []);
-
+  
   return (
     <>
-
+<Header />
 <CardDashboard/>
 
-{/*
-<div className="container-cards-grid">
-         {cards.map((card) => (
-            <Card
-            key={card.id}
-            id= {card.id}
-            title = {card.title}
-            location = {card.location}
-            image= {card.image} />
-         ))}
-            {<button onClick={handleAdd}>Agregar</button>}
-         </div>*/}
     </>
   )
 }
