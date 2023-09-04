87102adbf2f88d91f617d55451aae44b4a206ce8
Sylvia Llorente <sylviall81@gmail.com>
2023-09-04 18:12:33 +0200

    nva rama printcard sin cambios

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -2,7 +2,7 @@
 import {React, useState, useEffect} from 'react'
 import CardDashboard from "../../components/card-dashboard/Card-dashboard"
 import NavBar from '../../components/navBar/Nav-Bar';
-import Detail from'../detailPage/DetailPage'
+
 
 
 export default function HomePage() {
