9cc9432e7d38ae700ba4c8ce9eac38c27d0e4561
Sylvia Llorente <sylviall81@gmail.com>
2023-09-04 18:24:22 +0200

    borrar use efefct de los import de home xq no se estaba usando

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,5 +1,5 @@
 
-import {React, useState, useEffect} from 'react'
+import {React} from 'react'
 import CardDashboard from "../../components/card-dashboard/Card-dashboard"
 import NavBar from '../../components/navBar/Nav-Bar';
 
