a7784222c650638c03976e737c44a753f331d07c
JacinemiUA <131624010+JacinemiUA@users.noreply.github.com>
2023-09-07 01:09:18 +0200

    Corrección rutas

--- src/application/Router.jsx
+++ src/application/Router.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from '../pages/homepage/HomePage.jsx';
 import Detail from '../components/detail/Detail.jsx';
-import DestinationForm from '../components/Destination/Destination.jsx';
+import DestinationForm from '../components/destination/Destination.jsx';
 import Login from '../pages/loginPage/loginPage.jsx';
 import RegisterPage from '../pages/registerPage/RegisterPage.jsx';
-//import Detail from '../components/detail/Detail.jsx';
 import Edit from '../pages/editPage/EditPage.jsx';
 
 
@@ -18,9 +17,7 @@ function Router() {
         <Route path='/destination' element={<DestinationForm/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path="/" element= {<HomePage/>}/> 
-        {/*<Route path="/detail/:id" element={<Detail/>}/>*/}
         <Route path="/register" element= {<RegisterPage/>}/>
-        
         <Route path="/:id/edit" element= {<Edit/>}/>
     </Routes>
   );
