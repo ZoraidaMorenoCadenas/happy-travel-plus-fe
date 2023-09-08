a1c957ff863f73d9f0b3608b4cf5578f239ce984
ZoraidaMorenoCadenas <zoraidamorenocadenas79@gmail.com>
2023-09-08 09:31:09 +0200

    refactor

--- src/services/Cards-Service.jsx
+++ src/services/Cards-Service.jsx
@@ -13,15 +13,27 @@ export const CardService = () => {
     const response = axios.get(urlGetAll);
     return response;
   };
-  const getById = (id) => {
+  const getById = async (id) => {
     const response = axios.get(`${urlGetAll}/${id}`);
     return response;
   };
+
+  const postDestination = async (id, formData) => {
+    try {
+      return await axios.post(`${urlGetAll}/${id}`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data", 
+          "Accept": "application/json",
+        },
+      });
+    } catch (error) {
+      throw error;
+    }
+  };
  
   return {
-    
     getAll,
     getById,
-  
+    postDestination,
   }
 }
